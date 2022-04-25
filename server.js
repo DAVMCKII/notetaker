31dce3d9824aacf8faa77193d00c296e3bee0a5b
DAVMCKII <mckdav98@gmail.com>
2022-04-25 12:52:00 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const apiRoutes = require('./Develop/routes/apiRoutes')
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.static('./develop/public'))
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
